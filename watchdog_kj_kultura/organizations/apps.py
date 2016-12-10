521cc276f7cb09e2233e7b072467d90e1f9c3806
Adam Dobrawy <naczelnik@jawnosc.tk>
2016-12-10 21:40:13 +0100

    Add translation app_label of organizations and users

--- watchdog_kj_kultura/organizations/apps.py
+++ watchdog_kj_kultura/organizations/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class OrganizationsConfig(AppConfig):
-    name = 'organizations'
+    name = 'watchdog_kj_kultura.organizations'
+    verbose_name = _("Organizations system")
--- watchdog_kj_kultura/users/apps.py
+++ watchdog_kj_kultura/users/apps.py
@@ -1,9 +1,10 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class UsersConfig(AppConfig):
     name = 'watchdog_kj_kultura.users'
-    verbose_name = "Users"
+    verbose_name = _("Users")
 
     def ready(self):
         """Override this to put in:
