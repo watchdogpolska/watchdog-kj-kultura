7ca0c1f34dc69a974a7a0e6d432844ca089dfad8
Adam Dobrawy <naczelnik@jawnosc.tk>
2016-12-12 23:25:04 +0100

    Add tests to BreadcrumbsMixin

--- watchdog_kj_kultura/main/tests.py
+++ watchdog_kj_kultura/main/tests.py
@@ -1,9 +1,12 @@
-from django.test import TestCase
+from django.test import RequestFactory, TestCase
+from django.views.generic import TemplateView
 
 from ..users.factories import UserFactory
 from ..users.models import User
 from .templatetags.main_tags import (object_list_to_url_edit_obj,
                                      object_to_url_edit_obj)
+from .views import BreadcrumbsMixin
+from django.core.exceptions import ImproperlyConfigured
 
 
 class TempleTagsTestCase(TestCase):
@@ -15,3 +18,16 @@ class TempleTagsTestCase(TestCase):
     def test_object_list_to_url_edit_obj(self):
         qs = User.objects.all()
         self.assertEqual(object_list_to_url_edit_obj(qs), '/admin/users/user/')
+
+
+class BreadcrumbsMixinTestCase(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.request = self.factory.get('/customer/details')
+
+    def test_raises_missing_breadcrumbs(self):
+        class CustomView(BreadcrumbsMixin, TemplateView):
+            pass
+        with self.assertRaises(ImproperlyConfigured):
+            CustomView.as_view()(self.request)
